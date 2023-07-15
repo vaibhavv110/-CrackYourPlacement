e22fec4b42151a2bfd238f12921347560090d5f5
VAIBHAV VIMAL <133095780+vaibhavv110@users.noreply.github.com>
2023-07-15 11:02:47 +0530

    Create majority elemrnt.cpp

---
+++ majority elemrnt.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto it:m)
+        {
+            if(it.second>n/2)
+            return it.first;
+        }
+        return -1;
+    }
+};
