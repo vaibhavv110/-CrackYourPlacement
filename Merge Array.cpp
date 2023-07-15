fa79b6675a2f7fe067a35d2bc70877e9e0409826
VAIBHAV VIMAL <133095780+vaibhavv110@users.noreply.github.com>
2023-07-15 10:58:56 +0530

    Create Merge Array.cpp

---
+++ Merge Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+     
+
+        int left = m-1;
+        int right = 0;
+
+        while(left >= 0 && right < n){
+            if(nums1[left] > nums2[right]){
+                swap(nums1[left], nums2[right]);
+                left--;
+                right++;
+            }
+            else{
+                break;
+            }
+        }
+        sort(nums1.begin(), nums1.begin()+m); // Sorting till m^th index
+        sort(nums2.begin(), nums2.end());
+
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[i-m];
+        }
+    }
+};
